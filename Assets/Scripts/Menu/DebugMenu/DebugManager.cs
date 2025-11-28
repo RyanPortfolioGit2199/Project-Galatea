17e4f7965526cdb93ff45dfdeafd0b19d36e7009
RyanPortfolioGit2199 <ryan.allen.2199@gmail.com>
2025-11-27 20:40:53 -0800

    DebugUpdate2
    Need to finish the visuals for the rest UI then to work on the functionality in the script

--- Assets/Scripts/Menu/DebugMenu/DebugManager.cs
+++ Assets/Scripts/Menu/DebugMenu/DebugManager.cs
@@ -1,16 +1,31 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.InputSystem;
 
 public class DebugManager : MonoBehaviour
 {
+
+    [SerializeField] bool debugEnabled = false;
+
+    InputAction debugAction;
+    string debugMenu = "DebugMenu";
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        debugAction = InputSystem.actions.FindAction(debugMenu);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (debugAction.WasReleasedThisFrame() && !debugEnabled)
+        {
+            Debug.Log("Open Debug Menu");
+            debugEnabled = true;
+        }
+        else if (debugAction.WasReleasedThisFrame() && debugEnabled)
+        {
+            Debug.Log("Close Debug Menu");
+        }
     }
 }
