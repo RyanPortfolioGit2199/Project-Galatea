73b2cc6301aa3af1973b0f9d3c3efaa798f31700
RyanPortfolioGit2199 <ryan.allen.2199@gmail.com>
2025-12-27 15:59:11 -0800

    Debug Update
    updated 1 of the scripts

--- Assets/Scripts/Menu/DebugMenu/DebugUpgradeUIHandler.cs
+++ Assets/Scripts/Menu/DebugMenu/DebugUpgradeUIHandler.cs
@@ -3,13 +3,7 @@ using UnityEngine;
 public class DebugUpgradeUIHandler : MonoBehaviour
 {
     [SerializeField] GameObject debugMenu;
-    [SerializeField] GameObject debugUpgradeMenu;
-
-    public void DebugUpgradeMenu()
-    {
-        debugUpgradeMenu.SetActive(true);
-        debugMenu.SetActive(false);
-    }
+    
 
     
 }
