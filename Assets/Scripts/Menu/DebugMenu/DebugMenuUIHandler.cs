702f1a31c0b3c416e410aed4085b6a01430ff81b
RyanPortfolioGit2199 <ryan.allen.2199@gmail.com>
2025-12-27 15:58:09 -0800

    Debug Manager Update
    Added Debug Upgrades to the Debug Manager

--- Assets/Scripts/Menu/DebugMenu/DebugMenuUIHandler.cs
+++ Assets/Scripts/Menu/DebugMenu/DebugMenuUIHandler.cs
@@ -7,10 +7,18 @@ public class DebugMenuUIHandler : MonoBehaviour
     
     [SerializeField] GameObject debuglevelMenu;
     [SerializeField] GameObject debugMenu;
+    [SerializeField] GameObject debugUpgradeMenu;
 
     public void DebugLevelMenu()
     {
         debuglevelMenu.SetActive(false);
         debuglevelMenu.SetActive(true);
+    }
+    
+
+    public void DebugUpgradeMenu()
+    {
+        debugUpgradeMenu.SetActive(true);
+        debugMenu.SetActive(false);
     }
 }
