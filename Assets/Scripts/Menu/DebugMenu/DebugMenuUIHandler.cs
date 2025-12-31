3c34024dfe6501323eee16f81c42e8fbbd01ed9c
RyanPortfolioGit2199 <ryan.allen.2199@gmail.com>
2025-12-30 17:16:25 -0800

    Change Revert Test

--- Assets/Scripts/Menu/DebugMenu/DebugMenuUIHandler.cs
+++ Assets/Scripts/Menu/DebugMenu/DebugMenuUIHandler.cs
@@ -7,18 +7,10 @@ public class DebugMenuUIHandler : MonoBehaviour
     
     [SerializeField] GameObject debuglevelMenu;
     [SerializeField] GameObject debugMenu;
-    [SerializeField] GameObject debugUpgradeMenu;
 
     public void DebugLevelMenu()
     {
         debuglevelMenu.SetActive(false);
         debuglevelMenu.SetActive(true);
-    }
-    
-
-    public void DebugUpgradeMenu()
-    {
-        debugUpgradeMenu.SetActive(true);
-        debugMenu.SetActive(false);
     }
 }
