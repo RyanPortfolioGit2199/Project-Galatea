61aaf04c7a5ef9315862cbe6784f3644b2a292bc
RyanPortfolioGit2199 <ryan.allen.2199@gmail.com>
2026-01-11 12:13:48 -0800

    State Machine Update
    Added Idle State Logic
    
    Added Debug Text for which state the enemy is in.

---
+++ Assets/Scriptable Objects/Enemy/EnemySO.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "EnemySO", menuName = "Scriptable Objects/EnemySO")]
+public class EnemySO : ScriptableObject
+{
+    public float distanceFromPlayer;
+    public float attackRange;
+}
--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -1,16 +1,69 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour
 {
+    [SerializeField] EnemySO enemySO;
+    [SerializeField] TextMeshProUGUI stateNote;
+    private StateMachine brain;
+    private NavMeshAgent agent;
+    private PlayerController player;
+    private bool playerIsNear;
+    private bool withinAttackRange;
+    private float attackTimer;
+    private float changeMind;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        brain = GetComponent<StateMachine>();
+        agent = GetComponent<NavMeshAgent>();
+        player = FindAnyObjectByType<PlayerController>();
+
+        playerIsNear = false;
+        withinAttackRange = false;
+        brain.PushState(Idle, OnIdleEnter, OnIdleExit);
     }
 
     // Update is called once per frame
     void Update()
     {
+        playerIsNear = Vector3.Distance(this.transform.position, player.transform.position) < enemySO.distanceFromPlayer;
+        withinAttackRange = Vector3.Distance(this.transform.position, player.transform.position) < enemySO.attackRange;
+    }
+
+    
+    void OnIdleEnter()
+    {
+        agent.ResetPath();
+        stateNote.text = "Idle";
+    }
+
+    void Idle()
+    {
+        changeMind -= Time.deltaTime;
+        Debug.Log(changeMind);
+
+        if (playerIsNear)
+        {
+            //change to Chase State
+            Debug.Log("Enemy: I can see the player, I am going to Chase you.");
+        }
+        else if(changeMind <= 0)
+        {
+            //change to Wander State
+            Debug.Log("Enemy: The Player isn't in site. Going on Patrol.");
+        }
+    }
+
+    void OnIdleExit()
+    {
         
+    }
+
+    void OnPatrolEnter()
+    {
+        stateNote.text = "Patrol";
     }
 }
