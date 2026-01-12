3e41e24b0a6912ce442db09844713f68c059fc20
RyanPortfolioGit2199 <ryan.allen.2199@gmail.com>
2026-01-11 19:10:50 -0800

    Update
    Creating Idle, Patrol and Attack States for Enemy

--- Assets/Scriptable Objects/Enemy/EnemySO.cs
+++ Assets/Scriptable Objects/Enemy/EnemySO.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "EnemySO", menuName = "Scriptable Objects/EnemySO")]
 public class EnemySO : ScriptableObject
 {
-    public float distanceFromPlayer;
-    public float attackRange;
+    public int distanceFromPlayer;
+    public int attackRange;
+    public float fireRate;
 }
--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -43,17 +43,18 @@ public class Enemy : MonoBehaviour
     void Idle()
     {
         changeMind -= Time.deltaTime;
-        Debug.Log(changeMind);
+        //Debug.Log(changeMind);
 
         if (playerIsNear)
         {
-            //change to Chase State
+            brain.PushState(Chase, OnChaseEnter, OnChaseExit);
             Debug.Log("Enemy: I can see the player, I am going to Chase you.");
         }
-        else if(changeMind <= 0)
+        if(changeMind <= 0)
         {
-            //change to Wander State
+            brain.PushState(Patrol, OnPatrolEnter, OnPatrolExit);
             Debug.Log("Enemy: The Player isn't in site. Going on Patrol.");
+            changeMind = Random.Range(4, 10);
         }
     }
 
@@ -62,8 +63,83 @@ public class Enemy : MonoBehaviour
         
     }
 
+    void OnChaseEnter()
+    {
+        stateNote.text = "Chase";
+    }
+
+    void Chase()
+    {
+        agent.SetDestination(player.transform.position);
+        if(Vector3.Distance(this.transform.position, player.transform.position) > 5.5f)
+        {
+            brain.PushState(Idle, OnIdleEnter, OnIdleExit);
+        }
+        if (withinAttackRange)
+        {
+            brain.PushState(Attack, OnAttackEnter, null);
+        }
+    }
+
+    void OnChaseExit()
+    {
+        
+    }
+
     void OnPatrolEnter()
     {
         stateNote.text = "Patrol";
+
+        Vector3 wanderDistance = (Random.insideUnitSphere * 4f) + transform.position;
+        NavMeshHit navMeshHit;
+
+
+        NavMesh.SamplePosition(wanderDistance, out navMeshHit, 1f, NavMesh.AllAreas);
+
+        Vector3 destination = navMeshHit.position;
+
+        Debug.Log(destination);
+
+        agent.SetDestination(destination);
     }
+
+    void Patrol()
+    {
+        if(agent.remainingDistance <= .25f)
+        {
+            agent.ResetPath();
+            brain.PushState(Idle, OnIdleEnter, OnIdleExit);
+        }
+        if (playerIsNear)
+        {
+            brain.PushState(Chase, OnChaseEnter, OnChaseExit);
+        }
+    }
+
+    void OnPatrolExit()
+    {
+        
+    }
+
+    void OnAttackEnter()
+    {
+        agent.ResetPath();
+        stateNote.text = "Attack";
+    }
+
+    void Attack()
+    {
+        attackTimer -= Time.deltaTime;
+        if (!withinAttackRange)
+        {
+            brain.PopState();
+        }
+        else if (attackTimer <= 0)
+        {
+            Debug.Log("Enemy: Pew Pew Pew firing my gun at the Player!!!");
+            attackTimer = enemySO.fireRate;
+        }
+    }
+
+
 }
